d12cd8661f7541e1150f5b01a13abc88899a8576
nevenabgd <nevenabgd@gmail.com>
2020-09-21 21:42:23 -0700

    bugfix

--- spark_submit_common_crawl_copy.sh
+++ spark_submit_common_crawl_copy.sh
@@ -39,4 +39,17 @@ time spark-submit \
     output7 \
     --num_output_partitions 1 \
     --s3_output_path s3a://dataeng-bucket/crawlerdata/twitter
-
+	
+	
+time spark-submit \
+    --packages org.apache.hadoop:hadoop-aws:3.2.0 \
+	--py-files sparkcc.py \
+	--conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
+	--conf spark.hadoop.fs.s3a.access.key=$ACCESS_KEY \
+	--conf spark.hadoop.fs.s3a.secret.key=$SECRET_KEY \
+    ./common_crawl_extract.py \
+    --query "SELECT url, warc_filename, warc_record_offset, warc_record_length FROM ccindex WHERE crawl = 'CC-MAIN-2020-16' AND subset = 'warc' AND url_host_tld = 'is' LIMIT 100" \
+    s3a://commoncrawl/cc-index/table/cc-main/warc/ \
+    output7 \
+    --num_output_partitions 1 \
+    --s3_output_path s3a://dataeng-bucket/crawlerdata/twitter
