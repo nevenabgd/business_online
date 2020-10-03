aaacc496d0ecc49c0dee26be61a57709fe624a2f
nevenabgd <nevenabgd@gmail.com>
2020-10-03 12:46:39 -0700

    compare in ui

--- ui/dashui.py
+++ ui/dashui.py
@@ -13,46 +13,65 @@ app = dash.Dash('Business Online')
 # db connection
 CONN = None
 
-app.layout = html.Div([
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'Microsoft', 'value': 'Microsoft'},
-            {'label': 'Tesla', 'value': 'Tesla'},
-            {'label': 'Apple', 'value': 'Apple'}
-        ],
-        value='Microsoft',
-        clearable=False
-    ),
-    dcc.Graph(id='my-graph')
-], style={'width': '10'})
+@app.callback(Output('my-graph', 'figure'), Output('my-graph2', 'figure'), [Input('my-dropdown', 'value')])
+def update_graph(selected_dropdown_value):  
+    companies = selected_dropdown_value
+    print(companies)
+    if not companies:
+        companies = [""]
 
-@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
-def update_graph(selected_dropdown_value):
-    company_name = selected_dropdown_value
+    data_mentions = []
+    data_sentiment = []
 
-    cursor = CONN.cursor()
-    cursor.execute("select date, metric_value from company_metrics where company_name=%s and metric_name = 'mentions' order by date asc", (company_name) )
+    for company_name in companies:
+        print(company_name)
+        cursor = CONN.cursor()
+        cursor.execute("select date, metric_value from company_metrics where company_name=%s and metric_name = 'mentions' order by date asc", (company_name,))
+        dates = []
+        values = []
+        for (date, value) in cursor:
+            print("Result is {}, {}".format(date, value))
+            dates.append(date)
+            values.append(value)
+        df_mentions = pd.DataFrame({ "date": dates, "mentions": values })
 
-    dates = []
-    values = []
+        data_mentions.append(
+            {
+                'name': company_name,
+                'x': df_mentions.date,
+                'y': df_mentions.mentions,
+                'type': 'bar'
+            }
+        )
 
-    for (date, value) in cursor:
-        print("Result is {}, {}".format(date, value))
-        dates.append(date)
-        values.append(value)
+        cursor.execute("select date, metric_value from company_metrics where company_name=%s and metric_name = 'sentiment' order by date asc", (company_name,))
+        dates = []
+        values = []
+        for (date, value) in cursor:
+            print("Result is {}, {}".format(date, value))
+            dates.append(date)
+            values.append(value)
+        df_sentiment = pd.DataFrame({ "date": dates, "sentiment": values })
+
+        data_sentiment.append(
+            {
+                'name': company_name,
+                'x': df_sentiment.date,
+                'y': df_sentiment.sentiment,
+                'type': 'bar'
+            }
+        )
 
     cursor.close()
 
-    df = pd.DataFrame({ "date": dates, "mentions": values })
-
-    return {
-        'data': [{
-            'x': df.date,
-            'y': df.mentions
-        }],
-        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
-    }
+    return ({
+        'data': data_mentions,
+        'layout': {'margin': {'l': 100, 'r': 100, 't': 100, 'b': 100}, 'title': 'Mentions'}
+    },
+    {
+        'data': data_sentiment,
+        'layout': {'margin': {'l': 100, 'r': 100, 't': 100, 'b': 100}, 'title': 'Sentiment'}
+    })
 
 app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
@@ -84,18 +103,23 @@ if __name__ == '__main__':
     cursor.execute("select distinct company_name from company_metrics order by company_name asc")
 
     options = []
-    for company in cursor:
+    for row in cursor:
+        company = str(row[0])
         options.append({'label': company, 'value': company})
     cursor.close()
 
     app.layout = html.Div([
-        dcc.Dropdown(
-            id='my-dropdown',
-            options=options,
-            placeholder="Select a company",
-            clearable=False
-        ),
-        dcc.Graph(id='my-graph')
+        html.Div([
+            dcc.Dropdown(
+                id='my-dropdown',
+                options=options,
+                multi=True,
+                placeholder='Select companies...')
+        ], style={'display': 'inline-block', 'height': '40px', 'width': '600px'}),
+        html.Div([
+            dcc.Graph(id='my-graph'),
+            dcc.Graph(id='my-graph2')
+        ])
     ], style={'width': '10'})
 
     app.run_server()
