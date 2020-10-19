264e201d7ef5baa2cd4626fdbc48f8f240aa33e2
nevenabgd <nevenabgd@gmail.com>
2020-10-19 15:04:27 -0700

    polishing code

--- airflow/pipeline.py
+++ airflow/pipeline.py
@@ -22,24 +22,11 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': days_ago(2),
-    'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'sla': timedelta(hours=2),
-    'execution_timeout': timedelta(minutes=60),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'sla_miss_callback': yet_another_function,
-    # 'trigger_rule': 'all_success'
+    'execution_timeout': timedelta(minutes=60)
 }
 dag = DAG(
     'pipeline',
